144a6ad2d0fe96b7076c785737bea013e53f181f
Anchorite <ai_anchorite@proton.me>
2025-04-08 15:03:11 +1200

    Update install.js

--- install.js
+++ install.js
@@ -15,8 +15,10 @@ module.exports = {
       params: {
         venv: "env",               
         path: "app",
-        message:"uv pip install -r requirements.txt",
-                "uv pip install pydantic==2.10.6",
+        message:[
+        "uv pip install -r requirements.txt",
+        "uv pip install pydantic==2.10.6",
+        ]
       }
     },
   ]
