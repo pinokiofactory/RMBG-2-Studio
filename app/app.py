96146cd9766099c7964acae648d50dd386f3e159
ai_anchorite <ai_anchorite@proton.me>
2025-04-12 18:31:30 +1200

    Update app.py

--- app/app.py
+++ app/app.py
@@ -38,7 +38,7 @@ device = devicetorch.get(torch)
 torch.set_float32_matmul_precision(["high", "highest"][0])
 
 birefnet = AutoModelForImageSegmentation.from_pretrained(
-    "briaai/RMBG-2.0", trust_remote_code=True
+    "cocktailpeanut/rm", trust_remote_code=True
 )
 birefnet = devicetorch.to(torch, birefnet)
 
