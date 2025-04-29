a9d15958c4a3d164ef9808a25ca99b35515cdd56
6Morpheus6 <wanton-decay@gmx.net>
2025-04-29 04:12:09 +0200

    save disk space, script version 3.7

--- pinokio.js
+++ pinokio.js
@@ -1,6 +1,6 @@
 const path = require('path')
 module.exports = {
-  version: "3.6",
+  version: "3.7",
   title: "RMBG-2-Studio",
   description: "Enhanced background remove and replace app built around BRIA-RMBG-2.0 https://huggingface.co/briaai/RMBG-2.0",
   icon: "icon.png",
@@ -11,7 +11,8 @@ module.exports = {
       start: info.running("start.js"),
       update: info.running("update.js"),
       reset: info.running("reset.js"),
-	  cache: info.running("delete-cache.js")
+	    cache: info.running("delete-cache.js"),
+      link: info.running("link.js")
     }
     if (running.install) {
       return [{
@@ -55,13 +56,20 @@ module.exports = {
           icon: 'fa-solid fa-terminal',
           text: "Resetting",
           href: "reset.js",
-		}]
-	  } else if (running.cache) {
+        }]
+      } else if (running.cache) {
         return [{
           default: true,
           icon: 'fa-solid fa-terminal',
           text: "Clearing Cache",
           href: "delete-cache.js",
+        }]
+      } else if (running.link) {
+        return [{
+          default: true,
+          icon: 'fa-solid fa-terminal',
+          text: "Deduplicating",
+          href: "link.js",
         }]
       } else {
         return [{
@@ -81,11 +89,15 @@ module.exports = {
           icon: "fa-regular fa-circle-xmark",
           text: "Reset",
           href: "reset.js",
-		  confirm: "Are you sure you wish to reset this app?",
-	    }, {
+          confirm: "Are you sure you wish to reset this app?",
+        }, {
           icon: "fa-regular fa-circle-xmark",
           text: "Delete Gradio Image Cache",
           href: "delete-cache.js",
+        }, {
+          icon: "fa-solid fa-file-zipper",
+          text: "<div><strong>Save Disk Space</strong><div>Deduplicates redundant library files</div></div>",
+          href: "link.js",
         }]
       }
     } else {
